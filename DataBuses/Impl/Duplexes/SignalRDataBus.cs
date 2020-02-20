7b0e19216916b862458e3719d4373f605a5ac189
akboyd88 <akboyd88@gmail.com>
2020-02-20 16:47:43 -0600

    added documentation headers for Duplex SignalRDataBus and TcpClientDataBus

--- DataBuses/Impl/Duplexes/SignalRDataBus.cs
+++ DataBuses/Impl/Duplexes/SignalRDataBus.cs
@@ -10,41 +10,42 @@ namespace Boyd.DataBuses.Impl.Duplexes
 {
     
     /// <summary>
+    /// Generic exception for anything thrown by the SignalRDataBus
     /// </summary>
     public class SignalRDataBusException : Exception
     {
+
         /// <summary>
+        /// Create a new SignalRDataBusException with a custom message
         /// </summary>
+        /// <param name="message">custom exception message</param>
         public SignalRDataBusException(string message) : base(message)
         {
 
         }
     }
     
+
     /// <summary>
-    /// 
+    /// SignalR Data Bus that takes a configurable invoke receipient to receive messages and invoke target to send messages
+    /// to the signal r hub 
     /// </summary>
-    /// <typeparam name="T1"></typeparam>
-    /// <typeparam name="T2"></typeparam>
+    /// <typeparam name="T1">Outoing data type</typeparam>
+    /// <typeparam name="T2">Incoming data type</typeparam>
     internal class SignalRDataBus<T1, T2> : BaseDataBus<T1, T2>
     {
-        /// <summary>
-        /// 
-        /// </summary>
+        
         private readonly HubConnection _hubConnection;
-
-        /// <summary>
-        /// 
-        /// </summary>
         private readonly string _hubInvokeTarget;
-
         private volatile bool _isDisposed;
 
+
         /// <summary>
-        /// 
+        /// Create a new SignalRDataBus instance
         /// </summary>
-        /// <param name="options"></param>
-        /// <param name="loggerFactory"></param>
+        /// <param name="options">DataBus options specifying what signal r server to connect to, what remote method to invoke and what client event 
+        /// to list for</param>
+        /// <param name="loggerFactory">Logger factory that provides a ILogger instance to this instance for logging informatoin</param>
         public SignalRDataBus(DataBusOptions options, ILoggerFactory loggerFactory) : base(options, loggerFactory)
         {
 
@@ -64,7 +65,12 @@ namespace Boyd.DataBuses.Impl.Duplexes
             _hubConnection.StartAsync().Wait();
         }
         
+
         
+        /// <summary>
+        /// Dispose resources that implement IDisposable or need to be cleaned up such as connections
+        /// </summary>
+        /// <param name="disposing">if resources in this object should be cleaned up</param>
         protected override void Dispose(bool disposing)
         {
             if (_isDisposed)
@@ -81,42 +87,44 @@ namespace Boyd.DataBuses.Impl.Duplexes
             base.Dispose(disposing);
         }
 
+
         /// <summary>
-        /// 
+        /// Incoming data handler that pushes messages into a message queue
         /// </summary>
-        /// <param name="data"></param>
+        /// <param name="data">Data sent from remote SignalR server</param>
         private void RecvData(T2 data)
         {
             AddToQueue(data);
         }
         
         /// <summary>
-        /// 
+        /// Send data to the remote SignalR server
         /// </summary>
-        /// <param name="data"></param>
-        /// <param name="token"></param>
-        /// <returns></returns>
+        /// <param name="data">data to send to server</param>
+        /// <param name="token">cancellation token to abort request</param>
+        /// <returns>Task that completes when invocation is complete</returns>
         protected override async Task SendData(T1 data, CancellationToken token)
         {
             await _hubConnection.InvokeAsync<T1>(_hubInvokeTarget, data, token);
         }
 
         /// <summary>
-        /// 
+        /// Get data from the message queue, waiting if no data is available yet
         /// </summary>
-        /// <param name="pObjTimeout"></param>
-        /// <param name="token"></param>
-        /// <returns></returns>
+        /// <param name="pObjTimeout">How long to wait to get data out of the message queue</param>
+        /// <param name="token">cancellation token to abort the process of getting the data from the queue</param>
+        /// <returns>A task that completes with the next data from the message queue</returns>
         protected override Task<T2> GetData(TimeSpan pObjTimeout, CancellationToken token)
         {
             return TakeFromQueue(pObjTimeout, token);
         }
 
+        
         /// <summary>
-        /// 
+        /// Required by the base class but not currently needed for this SignalR DataBus
         /// </summary>
-        /// <param name="token"></param>
-        /// <returns></returns>
+        /// <param name="token">cancellation token to abort the read task</param>
+        /// <returns>Completed Task</returns>
         protected override Task CreateReadTask(CancellationToken token)
         {
             return Task.CompletedTask;
--- DataBuses/Impl/Duplexes/TcpClientDataBus.cs
+++ DataBuses/Impl/Duplexes/TcpClientDataBus.cs
@@ -8,6 +8,13 @@ using Microsoft.Extensions.Logging;
 
 namespace Boyd.DataBuses.Impl.Duplexes
 {
+    /// <summary>
+    /// Duplex data bus that connects to a listening TCP socket server. Assumes 
+    /// exchange is message based and uses a provided serializer and deserializer
+    /// to pop and push messages off the network stream.
+    /// </summary>
+    /// <typeparam name="T1">Message type sent by the socket server</typeparam>
+    /// <typeparam name="T2">Message type socket server expects</typeparam>
     internal class TcpClientDataBus<T1, T2> : BaseDataBus<T1, T2>
     {
 
@@ -18,6 +25,14 @@ namespace Boyd.DataBuses.Impl.Duplexes
         private readonly ISerializer<T1> _serializer;
         private readonly IDeserializer<T2> _deserializer;
 
+        /// <summary>
+        /// TCP Data Bus Constructor
+        /// </summary>
+        /// <param name="dataBusOptions">Generic shared options</param>
+        /// <param name="serializer">serializer that converts outgoing objects to bytes</param>
+        /// <param name="deserializer">deserializer the converts incoming bytes to desired type</param>
+        /// <param name="loggerFactory">logger factory that provides ILogger for instance</param>
+        /// <returns></returns>
         public TcpClientDataBus(
             DataBusOptions dataBusOptions,
             ISerializer<T1> serializer,
@@ -32,11 +47,11 @@ namespace Boyd.DataBuses.Impl.Duplexes
         }
 
         /// <summary>
-        /// 
+        /// Send data to the socket server
         /// </summary>
-        /// <param name="data"></param>
-        /// <param name="token"></param>
-        /// <returns></returns>
+        /// <param name="data">Data to send to the socket server</param>
+        /// <param name="token">Cancellation token to abort send</param>
+        /// <returns>Task that completes with send result</returns>
         protected override async Task SendData(T1 data, CancellationToken token)
         {
             ReadOnlyMemory<byte> outData = _serializer.Serialize(data);
@@ -47,14 +62,20 @@ namespace Boyd.DataBuses.Impl.Duplexes
         /// <summary>
         /// Extracts a single object from the stream using the deserializer in a streaming manner
         /// </summary>
-        /// <param name="pObjTimeout"></param>
-        /// <param name="token"></param>
-        /// <returns></returns>
+        /// <param name="pObjTimeout">amount of time to wait to retrieve the messages from the stream</param>
+        /// <param name="token">cancellation to cancel call</param>
+        /// <returns>Task that completes with a new data object from the stream or is cancelled due to timeout</returns>
         protected override Task<T2> GetData(TimeSpan pObjTimeout, CancellationToken token)
         {
             return TakeFromQueue(pObjTimeout, token);
         }
 
+        /// <summary>
+        /// Create a read task that extracts data from the TCP network stream and maintains the socket connection, 
+        /// converts the data into the desired type and inserts it into the message buffer.
+        /// </summary>
+        /// <param name="token">CancellationToken to cancel the read task</param>
+        /// <returns>Task that doesn't complete until the databus is closed/disposed</returns>
         protected override Task CreateReadTask(CancellationToken token)
         {
             return Task.Run(async () =>
@@ -77,6 +98,12 @@ namespace Boyd.DataBuses.Impl.Duplexes
             }, token);
         }
 
+
+        /// <summary>
+        /// Clean up connections and other resources that implement IDispoable that were
+        /// allocated for this instance
+        /// </summary>
+        /// <param name="disposing"></param>
         protected override void Dispose(bool disposing)
         {
             if (_isDisposed)
